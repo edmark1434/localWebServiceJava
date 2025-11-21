26f257ec045777b9caa0f709ab47af523f2177e6
Edmark Talingting <edmarktalingting0@gmail.com>
2025-11-21 20:18:22 +0800

    fix fingerprint

--- src/main/java/com/example/coop/CoopApplication.java
+++ src/main/java/com/example/coop/CoopApplication.java
@@ -1,5 +1,6 @@
 package com.example.coop;
 
+import com.zkteco.biometric.FingerprintSensorEx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoopApplication.class, args);
 	}
 
